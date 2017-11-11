7cc8bc2685ef419f406dd32fb8cc9555a26d5767
Marcos Rodrigues <eu@marcosrodrigues.me>
2017-11-10 22:52:21 -0200

    feat: Animate Book mount and unmount

--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -1,24 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import Book from './Book';
 
 const Shelf = ({shelf, books, shelves, onChangeShelf}) => (
   <div className="bookshelf">
     <h2 className="bookshelf-title">{shelf.name}</h2>
     <div className="bookshelf-books">
-      <ol className="books-grid">
+      <TransitionGroup component="ol" className="books-grid">
         {books.filter(book => shelf.id === book.shelf)
           .map(book => (
-            <li key={book.id}>
-              <Book
-                book={book}
-                shelves={shelves}
-                onChangeShelf={onChangeShelf}
-              />
-            </li>
+              <CSSTransition
+                key={book.id}
+                appear={true}
+                timeout={{
+                 enter: 600,
+                 exit: 300,
+                }}
+                classNames="book"
+              >
+                <li>
+                  <Book
+                    book={book}
+                    shelves={shelves}
+                    onChangeShelf={onChangeShelf}
+                  />
+                </li>
+              </CSSTransition>
+
           )
         )}
-      </ol>
+      </TransitionGroup>
     </div>
   </div>
 );
