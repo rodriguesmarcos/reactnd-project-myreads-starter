2bb0922505e137f92498c45bd7c0705503683227
Marcos Rodrigues <eu@marcosrodrigues.me>
2017-11-11 22:29:06 -0200

    test: Add Shelves and Books test fixtures

---
+++ src/tests/fixtures/shelves.js
@@ -0,0 +1,7 @@
+const shelves = [
+    { id: 'currentlyReading', name: 'Currently Reading' },
+    { id: 'wantToRead', name: 'Want to Read' },
+    { id: 'read', name: 'Read' },
+];
+
+export default shelves;
